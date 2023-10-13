dc11f87239a13e4bacc59eeadcc63bda595adb9f
Alwin Doss <alwindoss84@gmail.com>
2023-10-13 14:24:19 +0530

    add assets
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

--- webapp/pages/index.vue
+++ webapp/pages/index.vue
@@ -3,8 +3,7 @@
         <h1 class="text-3xl font-bold underline">Welcome to Daft</h1>
         <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
             <div class="shrink-0">
-                <img class="h-12 w-12" src="https://freeiconshop.com/wp-content/uploads/edd/chat-flat.png"
-                    alt="ChitChat Logo">
+                <img class="h-12 w-12" src="~/assets/images/profile.jpg" alt="ChitChat Logo">
             </div>
             <div>
                 <div class="text-xl font-medium text-black">ChitChat</div>
